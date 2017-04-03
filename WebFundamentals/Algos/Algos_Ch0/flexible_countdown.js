5076fd03bb5215303728026311dfe2c3d0bd0a15
Giovanni <flipgio@gmail.com>
2017-04-02 21:39:24 -0700

    corrected flexible countdown

--- WebFundamentals/Algos/Algos_Ch0/flexible_countdown.js
+++ WebFundamentals/Algos/Algos_Ch0/flexible_countdown.js
@@ -1,8 +1,10 @@
 //given (lowNum, highNum, mult), print the numbers in multiples of mult from highNum down to lowNum, using a FOR loop.
 
 function countdown(lowNum, highNum, mult) {
-    for (var i = highNum; i >= lowNum; i-= mult) {
-        console.log(i);
+    for (var i = highNum; i >= lowNum; i--) {
+        if (i%mult === 0) {
+            console.log(i);
+        }
     }
 }
 
