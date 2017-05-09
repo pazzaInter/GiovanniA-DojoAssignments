6bab000d9c3ab49148e2e0409d4909bdb9f8c54b
Giovanni <flipgio@gmail.com>
2017-05-08 21:21:08 -0700

    finished MathDojo

--- Python/OOP/mathdojo.py
+++ Python/OOP/mathdojo.py
@@ -1,20 +1,28 @@
 class MathDojo(object):
-    """docstring for MathDojo."""
+    """calculator for adding and subtracting"""
     def __init__(self):
         self.result = 0
 
     def add(self, *args):
-        for each in args:
-            self.result += each
+        for x in args:
+            if isinstance(x, list) or isinstance(x, tuple):
+                for y in x:
+                    self.result += y
+            else:
+                self.result += x
         return self
     def subtract(self, *args):
-        for each in args:
-            self.result -= each
+        for x in args:
+            if isinstance(x, list) or isinstance(x, tuple):
+                for y in x:
+                    self.result -= y
+            else:
+                self.result -= x
         return self
 
 print MathDojo().add(2).add(2, 5).subtract(3, 2).result
-md = MathDojo()
-
-md.add(2).add(2, 5).subtract(3, 2).result
-
 print MathDojo().add([1],3,4).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract(2, [2,3], [1.1, 2.3]).result
+
+md = MathDojo()
+print md.add([1],3,4).add((3, 5, 7, 8), [2, 4.3, 1.25]).subtract(2, [2,3], [1.1, 2.3]).result
+print md.add(2).add(2, 5).subtract(3, 2).result
