0097b95171a35a0c26ab9e1c7ba98931a63868e9
Giovanni <flipgio@gmail.com>
2017-05-21 13:40:35 -0700

    updates books assigment for optional Authors

--- Python/Django/Books/apps/book/models.py
+++ Python/Django/Books/apps/book/models.py
@@ -3,9 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author)
     published_date = models.DateTimeField()
     category = models.CharField(max_length=30)
     in_print = models.BooleanField()
